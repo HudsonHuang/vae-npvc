d76bfdd90172e40c289fa7e3e4ac60920b1712c0
huang-zhongyi <790209714@qq.com>
2017-11-17 19:29:34 +0800

    edit all_default.sh for next run

--- all_default.sh
+++ all_default.sh
@@ -1,18 +1,18 @@
-# bash download.sh
+#bash download.sh
 source activate tensorflow
-# pip install -U pip
-# pip install -r requirements.txt
-# export LD_LIBRARY_PATH=/usr/local/cuda/lib64/
-# python analyzer.py
-# python build.py
-# python main.py \
-#   --model ConvVAE \
-#   --trainer VAETrainer \
-#   --architecture architecture-vae-vcc2016.json
+#pip install -U pip
+#pip install -r requirements.txt
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64/
+#python analyzer.py
+#python build.py
+python main.py \
+  --model ConvVAE \
+  --trainer VAETrainer \
+  --architecture architecture-vae-vcc2016.json
 python convert.py \
   --src SF1 \
   --trg TM3 \
   --model ConvVAE \
-  --checkpoint logdir/train/1117-1836-57-2017/model.ckpt-59253 \
+  --checkpoint logdir/train/[timestamp]/model.ckpt-[newest-ckpt-id] \
   --file_pattern "./dataset/vcc2016/bin/Testing Set/{}/*.bin"
-echo "Please find your results in `./logdir/output/1117-1919-00-2017`"
+echo "Please find your results in newest folder of `./logdir/output/"
